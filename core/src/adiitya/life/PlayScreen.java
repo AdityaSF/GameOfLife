2db75ca288e4216b23d60897c83f0e01fd8d0958
AdityaSF <A44itya@hotmail.com>
2018-09-03 13:06:07 -0600

    Add text rendering to draw info on screen

--- core/src/adiitya/life/PlayScreen.java
+++ core/src/adiitya/life/PlayScreen.java
@@ -3,8 +3,14 @@ package adiitya.life;
 import adiitya.life.input.GameInput;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator.FreeTypeFontParameter;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer.ShapeType;
+import com.badlogic.gdx.math.Vector3;
 
 public class PlayScreen implements Screen {
 
@@ -13,6 +19,10 @@ public class PlayScreen implements Screen {
 
 	private ShapeRenderer renderer;
 	private World world;
+
+	private OrthographicCamera textCam;
+	private SpriteBatch fontBatch;
+	private BitmapFont font;
 
 	private GameInput input;
 
@@ -25,10 +35,30 @@ public class PlayScreen implements Screen {
 
 		renderer = new ShapeRenderer();
 		world = new World(256, 256, 0.1F);
+
+		textCam = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		textCam.setToOrtho(true);
+		fontBatch = new SpriteBatch();
+		font = getFont();
+
 		elapsed = 0F;
 
 		input = new GameInput(game, world);
 		Gdx.input.setInputProcessor(input);
+	}
+
+	private BitmapFont getFont() {
+
+		FreeTypeFontGenerator gen = new FreeTypeFontGenerator(Gdx.files.internal("font.ttf"));
+		FreeTypeFontParameter param = new FreeTypeFontParameter();
+		param.size = 24;
+		param.flip = true;
+		param.genMipMaps = false;
+
+		BitmapFont font = gen.generateFont(param);
+		gen.dispose();
+
+		return font;
 	}
 
 	@Override
@@ -44,14 +74,21 @@ public class PlayScreen implements Screen {
 		world.render(renderer, game.getCamera());
 		renderer.end();
 
-		if (elapsed >= 1F) {
-			elapsed -= 1F;
-			Gdx.app.log("FPS", String.format("%s, POS: (%d, %d), GEN: %d", Gdx.graphics.getFramesPerSecond(), (int) game.getCamera().position.x / 16, (int) game.getCamera().position.y / 16, world.getGeneration()));
-		}
+		Vector3 tl = textCam.unproject(new Vector3(0, 0, 0));
+
+		fontBatch.begin();
+		font.draw(fontBatch, String.format("FPS: %d", Gdx.graphics.getFramesPerSecond()), tl.x + 10, tl.y + 10);
+		font.draw(fontBatch, String.format("Pos: (%d, %d)", (int) game.getCamera().position.x / 16, (int) -(game.getCamera().position.y / 16)), tl.x + 10, tl.y + 36);
+		fontBatch.end();
 	}
 
 	@Override
 	public void resize(int width, int height) {
+
+		textCam.viewportWidth = width;
+		textCam.viewportHeight = height;
+		textCam.update();
+
 		updateRenderer();
 	}
 
@@ -73,9 +110,12 @@ public class PlayScreen implements Screen {
 	@Override
 	public void dispose() {
 		renderer.dispose();
+		fontBatch.dispose();
+		font.dispose();
 	}
 
 	private void updateRenderer() {
 		renderer.setProjectionMatrix(game.getCamera().combined);
+		fontBatch.setProjectionMatrix(textCam.combined);
 	}
 }
